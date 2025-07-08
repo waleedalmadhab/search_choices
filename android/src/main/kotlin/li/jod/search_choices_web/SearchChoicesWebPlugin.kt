fb60d5cec9cac0dafda546ec2b4b26b0db961144
waleedalmadhab <63848165+waleedalmadhab@users.noreply.github.com>
2025-07-08 20:31:36 +0300

    Update SearchChoicesWebPlugin.kt

--- android/src/main/kotlin/li/jod/search_choices_web/SearchChoicesWebPlugin.kt
+++ android/src/main/kotlin/li/jod/search_choices_web/SearchChoicesWebPlugin.kt
@@ -6,7 +6,6 @@ import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
-import io.flutter.plugin.common.PluginRegistry.Registrar
 
 /** SearchChoicesWebPlugin */
 public class SearchChoicesWebPlugin: FlutterPlugin, MethodCallHandler {
@@ -30,13 +29,7 @@ public class SearchChoicesWebPlugin: FlutterPlugin, MethodCallHandler {
   // them functionally equivalent. Only one of onAttachedToEngine or registerWith will be called
   // depending on the user's project. onAttachedToEngine or registerWith must both be defined
   // in the same class.
-  companion object {
-    @JvmStatic
-    fun registerWith(registrar: Registrar) {
-      val channel = MethodChannel(registrar.messenger(), "search_choices_web")
-      channel.setMethodCallHandler(SearchChoicesWebPlugin())
-    }
-  }
+
 
   override fun onMethodCall(@NonNull call: MethodCall, @NonNull result: Result) {
     if (call.method == "getPlatformVersion") {
